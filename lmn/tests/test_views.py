bfac280542fde81e36effd9680d260bc81f8d22d
yp0975eu <yp0975eu@go.minneapolis.edu>
2020-11-25 09:57:31 -0600

    add test for user_profile

--- lmn/tests/test_views.py
+++ lmn/tests/test_views.py
@@ -495,3 +495,25 @@ class TestUserAuthentication(TestCase):
         new_user = authenticate(username='sam12345', password='feRpj4w4pso3az@1!2')
         self.assertRedirects(response, reverse('user_profile', kwargs={"user_pk": new_user.pk}))   
         self.assertContains(response, 'sam12345')  # page has user's name on it
+
+
+class TestMyUserProfile(TestCase):
+    fixtures = ['testing_users', 'testing_users_profile']
+
+    def test_user_not_logged_in_should_get_sent_to_login_page(self):
+        response = self.client.get(reverse('my_user_profile'))
+        self.assertRedirects(response, '/accounts/login/?next=/user/profile/')
+
+    def test_user_logged_in(self):
+        self.client.force_login(User.objects.first())
+        response = self.client.get(reverse('my_user_profile'))
+        self.assertTemplateUsed(response, 'lmn/users/my_user_profile.html')
+
+    def test_user_bio_is_displayed_on_public_profile_page(self):
+        response = self.client.get(reverse('user_profile', kwargs={'user_pk':1}))
+        self.assertContains(response, 'This bio should be avilable on the page for user 1')
+        self.assertTemplateUsed(response, 'lmn/users/user_profile.html')
+
+    def test_user_bio_is_not_on_other_users_profile_page(self):
+        response = self.client.get(reverse('user_profile', kwargs={'user_pk':2}))
+        self.assertNotContains(response, 'This bio should be avilable on the page for user 1')
